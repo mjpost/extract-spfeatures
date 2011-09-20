11d2b06a6aac98b71daf9c95520d6766a21468de
Matt Post <post@jhu.edu>
2011-09-20 09:32:09 -0400

    added some documentation

--- extract.pl
+++ extract.pl
@@ -14,13 +14,18 @@ use File::Basename;
 use Getopt::Long;
 
 # add in --local for local features only
-my $do_local_only = "";
+my $do_local_only = 0;
 my $mincount = 1;
 my $extract = dirname($0) . "/extract-spfeatures";
 
+# arguments: --min changes the feature pruning threshold (defaults to
+# 1), while --local turns off non-local features
 my $result = GetOptions("min=i" => \$mincount,
 						"local" => \$do_local_only);
 
+# read in the parses from STDIN.  two formats are permitted: plain
+# parse trees, with an optional score preceding the tree (delimited
+# with a tab).
 my (@scores,@parses);
 while (my $line = <>) {
   chomp($line);
@@ -42,12 +47,30 @@ while (my $line = <>) {
   push(@parses,$parse);
 }
 
-my $numlines = @scores;
-
+# get some temporary files.  C&J's feature extractor expects parallel
+# gold-standard parse trees in addition to the trees you send it, and
+# each tree set has to be in a very specific format.
 my ($parsefh, $parsefilename) = tempfile();
 my ($goldfh,  $goldfilename)  = tempfile();
 
-print $goldfh "$numlines\n";
+# generate the two files.  The parse tree file has three lines of
+# information for each parse tree candidate:
+#
+# NUMCANDIDATES [tab] SENTENCEID
+# CAND1 SCORE
+# CAND1 PARSE TREE
+# CAND2 SCORE
+# CAND2 PARSE TREE
+# ...
+#
+# The gold standard file has one line at the top indicating the number
+# of entries, and then a single line for each tree, in the format:
+#
+# SENTENCEID [tab] PARSE TREE
+#
+# Since we're not doing n-best reranking, we have just one item on
+# each n-best list.
+print $goldfh (scalar @scores) . $/;
 for (my $i = 0; $i <= $#scores; $i++) {
   print $parsefh "1\tid.$i\n";
   print $parsefh "$scores[$i]\n";
@@ -56,12 +79,18 @@ for (my $i = 0; $i <= $#scores; $i++) {
   print $goldfh "id.$i\t$parses[$i]\n";
 }
 
+# extract-spfeatures writes the feature file to the file indicated by
+# its third argument, and the feature mapping to STDOUT.  we have to
+# do further processing of both, so we temporarily save them to temp
+# files
 my ($featurefilename) = mktemp("/tmp/XXXX");
 my ($mapfilename) = mktemp("/tmp/XXXX");
 
-my $cmd = qq($extract -s $mincount -ciae $do_local_only "cat $parsefilename" "cat $goldfilename" $featurefilename > $mapfilename 2> /dev/null);
+my $local = ($do_local_only) ? "-f local" : "";
+my $cmd = qq($extract -s $mincount -ciae $local "cat $parsefilename" "cat $goldfilename" $featurefilename > $mapfilename 2> /dev/null);
 system($cmd);
 
+# munge the feature file output
 open FEATURES, $featurefilename;
 while ($_ = <FEATURES>) {
   chomp($_);
@@ -91,7 +120,7 @@ while ($_ = <FEATURES>) {
 }
 close(FEATURES);
 
-# print the features to STDERR (were written to STDOUT by extract-spfeatures)
+# now print the feature mapping to STDERR
 open MAP, $mapfilename;
 while (<MAP>) {
   print STDERR;
@@ -101,5 +130,4 @@ close(MAP);
 # cleanup
 close($goldfh);
 close($parsefh);
-
-map { unlink($file); } ($mapfilename,$goldfilename, $parsefilename);
+map { unlink($file); } ($featurefilename,$mapfilename,$goldfilename,$parsefilename);
